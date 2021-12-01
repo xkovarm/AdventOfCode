d9d40a1aa9ece636bd89ca4cfb23e5c98f64a886
Martin Kovar <martin.kovar@rws.com>
2021-12-01 10:04:24 +0100

    Day01 completed

---
+++ 2021/Day01/Puzzle.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode.Day01
+{
+    internal class Puzzle : PuzzleBase
+    {
+        // -- CALCULATION PARAMETERS AND RESULT ------------------------------------
+
+        private const int WindowSize = 3; // part 1: 1;
+
+        protected override string SampleResult => "5";  // part 1: "7";
+
+        // -------------------------------------------------------------------------
+
+        protected override Task<string> GetSampleInputAsync()
+        {
+            return File.ReadAllTextAsync(@"Day01\sample.txt");
+        }
+
+        protected override Task<string> GetCheckInputAsync()
+        {
+            return File.ReadAllTextAsync(@"Day01\input.txt");
+        }
+
+        protected override string DoCalculation(string input)
+        {
+            var values = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var result = 0;
+            var previous = -1;
+
+            for (int i = 0; i < values.Length - WindowSize + 1; i++)
+            {
+                var current = values[i..(i + WindowSize)].Sum();
+
+                if (current > previous && previous != -1)
+                {
+                    result++;
+                }
+
+                previous = current;
+            }
+
+            return result.ToString();
+        }
+    }
+}
