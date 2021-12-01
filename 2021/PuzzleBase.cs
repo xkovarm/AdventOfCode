0d6579ca09ca4e52e3d61ad803350fecf6fc89fc
Martin Kovar <martin.kovar@rws.com>
2021-12-01 10:04:10 +0100

    Common stuff

---
+++ 2021/PuzzleBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    internal abstract class PuzzleBase
+    {
+        protected abstract string SampleResult { get; }
+
+        protected abstract Task<string> GetSampleInputAsync();
+
+        protected abstract Task<string> GetCheckInputAsync();
+
+        protected abstract string DoCalculation(string input);
+
+        public async Task<string> CalculateAsync()
+        {
+            var input = await GetCheckInputAsync();
+
+            return DoCalculation(input);
+        }
+
+        public async Task AssertAsync()
+        {
+            var input = await GetSampleInputAsync();
+
+            var result = DoCalculation(input);
+
+            if (result != SampleResult)
+            {
+                throw new InvalidOperationException($"Assertion failed! Current result: {result}, expected: {SampleResult}");
+            }
+        }
+    }
+}
