01c79d0b8bac84a382f05e07d51569b29495a01d
Martin Kovar <martin.kovar@rws.com>
2022-12-02 09:39:50 +0100

    Day 02

---
+++ 2022/Day02/Puzzle.cs
@@ -0,0 +1,73 @@
+﻿namespace AdventOfCode.Day02
+{
+    internal class Puzzle : PuzzleBase
+    {
+        // -- CALCULATION PARAMETERS AND RESULT ------------------------------------
+
+        protected override string SampleResult => "12"; // PART 1: "15";
+
+        // -------------------------------------------------------------------------
+
+        protected override string DoCalculation(string input)
+        {
+            var rounds = input
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(l => l.Split(" "))
+                .Select(l => (l[0], l[1]))
+                .ToList();
+
+            var points = Play(rounds);
+
+            return points.ToString();
+        }
+
+        private int Play(IList<(string He, string Me)> rounds)
+        {
+            var points = 0;
+
+            foreach (var round in rounds)
+            {
+                // PART 1: var me = Translate1(round.Me);
+                var me = Translate2(round.Me, round.He);
+
+                points += me switch
+                {
+                    "A" => 1,
+                    "B" => 2,
+                    "C" => 3
+                };
+
+                if (round.He == me)
+                {
+                    points += 3;
+                } 
+                else if ((me, round.He) == ("A", "C") || (me, round.He) == ("B", "A") || (me, round.He) == ("C", "B")) 
+                {
+                    points += 6;
+                }
+            }
+
+            return points;
+
+            string Translate1(string me)
+            {
+                return me switch
+                {
+                    "X" => "A",
+                    "Y" => "B",
+                    "Z" => "C",
+                };
+            }
+
+            string Translate2(string me, string he)
+            {
+                return me switch
+                {
+                    "X" => he switch { "A" => "C", "B" => "A", "C" => "B" },
+                    "Y" => he switch { "A" => "A", "B" => "B", "C" => "C" },
+                    "Z" => he switch { "A" => "B", "B" => "C", "C" => "A" },
+                };
+            }
+        }
+    }
+}
--- 2022/PuzzleBase.cs
+++ 2022/PuzzleBase.cs
@@ -1,12 +1,11 @@
 ﻿using System.Text.RegularExpressions;
+using HtmlAgilityPack;
 
 namespace AdventOfCode
 {
     internal abstract class PuzzleBase
     {
         protected abstract string SampleResult { get; }
-
-        protected abstract Task<string> GetSampleInputAsync();
 
         protected abstract string DoCalculation(string input);
 
@@ -30,7 +29,26 @@ namespace AdventOfCode
             }
         }
 
-        protected virtual async Task<string> GetCheckInputAsync()
+        protected virtual Task<string> GetSampleInputAsync()
+        {
+            return GetFile("Day{0:D02}\\sample.txt", "https://adventofcode.com/2022/day/{0}",
+                content =>
+                {
+                    var doc = new HtmlDocument();
+                    doc.LoadHtml(content);
+
+                    var node = doc.DocumentNode.SelectSingleNode("//pre/code");
+
+                    return node.InnerText;
+                });
+        }
+
+        protected virtual Task<string> GetCheckInputAsync()
+        {
+            return GetFile("Day{0:D02}\\input.txt", "https://adventofcode.com/2022/day/{0}/input");
+        }
+
+        protected virtual async Task<string> GetFile(string targetFilePattern, string urlPattern, Func<string, string>? transformContent = null)
         {
             var dayMatch = Regex.Match(GetType().FullName, ".Day([0-9][0-9]).");
 
@@ -40,17 +58,20 @@ namespace AdventOfCode
             }
 
             var day = int.Parse(dayMatch.Groups[1].Value);
-            var filename = $"Day{day:D02}\\input.txt";
+            var filename = string.Format(targetFilePattern, day);
 
             if (!File.Exists(filename))
             {
                 var session = await File.ReadAllTextAsync(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".AoC.cookie"));
                 using var client = new HttpClient();
                 client.DefaultRequestHeaders.Add("Cookie", $"session={session.Trim()}");
-                using var response = await client.GetAsync($"https://adventofcode.com/2022/day/{day}/input");
+                using var response = await client.GetAsync(string.Format(urlPattern, day));
                 response.EnsureSuccessStatusCode();
 
                 var content = (await response.Content.ReadAsStringAsync()).Replace("\n", Environment.NewLine);
+
+                content = transformContent?.Invoke(content) ?? content;
+
                 Directory.CreateDirectory(Path.GetDirectoryName(filename));
                 await File.WriteAllTextAsync(filename, content);
 
