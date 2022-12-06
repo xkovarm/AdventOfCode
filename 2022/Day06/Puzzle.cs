00fd5db800f94de635fe66860da762c77d4aa4f3
Martin Kovar <martin.kovar@rws.com>
2022-12-06 08:25:57 +0100

    Day 06

---
+++ 2022/Day06/Puzzle.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode.Day06
+{
+    internal class Puzzle : PuzzleBase
+    {
+        // -- CALCULATION PARAMETERS AND RESULT ------------------------------------
+
+        protected override string SampleResult => "19"; // PART 1: "7"; 
+
+        // -------------------------------------------------------------------------
+
+        protected override string DoCalculation(string input)
+        {
+            // PART 1: return Calculate(input, 4).ToString();
+            return Calculate(input, 14).ToString();
+        }
+
+        private int Calculate(string input, int headerLength)
+        {
+            for (int i = 0; i < input.Length - headerLength; i++)
+            {
+                var distinctLength = input.Skip(i).Take(headerLength).Distinct().Count();
+                
+                if (distinctLength == headerLength)
+                {
+                    return i + headerLength;
+                }
+            }
+
+            return -1;
+        }        
+    }
+}
