8254e46aa7ca68ab523b9a770fe4e9180bfafe03
Martin Kovar <martin.kovar@rws.com>
2023-12-01 12:01:44 +0100

    Day 01

--- 2023/Day01/Puzzle.cs
+++ 2023/Day01/Puzzle.cs
@@ -1,15 +1,76 @@
-﻿namespace AdventOfCode.Day01;
+﻿using MoreLinq;
+
+namespace AdventOfCode.Day01;
 
 internal class Puzzle : PuzzleBase
 {
     // -- CALCULATION PARAMETERS AND RESULT ------------------------------------
 
-    protected override string SampleResult => "";
+    protected override string SampleResult => "281";
+
+    private string[] Words = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
     // -------------------------------------------------------------------------
 
+    protected override Task<string> GetSampleInputAsync()
+    {
+        return Task.FromResult(@"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen");
+    }
+
     protected override string DoCalculation(string input)
     {
-        throw new NotImplementedException();
+        var sum = input
+            .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(GetNumber)
+            .Select(int.Parse)
+            .Sum();
+
+        return sum.ToString();
+    }
+
+    private string GetNumber(string input) 
+    {
+        var span = input.AsSpan();
+
+        return $"{Scan(span, true)}{Scan(span, false)}";
+
+        int Scan(ReadOnlySpan<char> span, bool forward)
+        {
+            for (int i = forward ? 0 : input.Length - 1; i < input.Length && forward || i >= 0 && !forward ; i += forward ? 1 : -1)
+            {
+                var found = CheckLiteral(span.Slice(i));
+
+                if (found.HasValue)
+                {
+                    return found.Value;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        int? CheckLiteral(ReadOnlySpan<char> span)
+        {
+            if (span[0] >= '0' && span[0] <= '9')
+            {
+                return span[0] - '0';
+            }
+
+            for (int i = 0; i < Words.Length; i++)
+            {
+                if (span.StartsWith(Words[i]))
+                {
+                    return i + 1;
+                }
+            }
+
+            return null;
+        }
     }
 }
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using AdventOfCode;
 
-var currentDay = 1;// DateOnly.FromDateTime(DateTime.UtcNow).Day;
+var currentDay = DateOnly.FromDateTime(DateTime.UtcNow).Day;
 
 Console.WriteLine($"Current day: {currentDay}");
 
